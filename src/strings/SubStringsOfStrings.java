8d413be2d7528a09e4b04a81cb72860083c1218e
reena-10 <reenamahto310@gmail.com>
2026-03-06 20:57:21 +0530

    Feb 06

--- src/strings/SubStringsOfStrings.java
+++ src/strings/SubStringsOfStrings.java
@@ -6,5 +6,13 @@ public class SubStringsOfStrings {
         System.out.println(s.substring(3));
         System.out.println(s.substring(1,6));
         System.out.println(s.substring(0,s.length()));
+        //print all substring
+        String str = "gopi";
+        for(int i =0;i<=str.length();i++){
+            for(int j=i+1; j<=str.length();j++){
+                System.out.print(str.substring(i,j)+" ");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ src/strings/sumOfSubStrings.java
@@ -0,0 +1,7 @@
+package strings;
+
+public class sumOfSubStrings {
+    public static void main(String args[]){
+
+    }
+}
